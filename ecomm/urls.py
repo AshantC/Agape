8e02551a65d632bacbe676e24b25b19fa450ff4b
AshantC <ashantchaudhary22@gmail.com>
2024-06-27 21:40:50 +0545

    template added

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -14,9 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# for image files
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path('accounts/', include("accounts.urls")),
+    path("", include("products.urls")),
     path('admin/', admin.site.urls),
 ]
+
+# for image files
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    
+urlpatterns += staticfiles_urlpatterns()
